9ffa77f465cbfbb6250e5684041b6b5538ae6e9a
Erik Dubois <erik.dubois@gmail.com>
2017-07-20 20:52:11 +0200

    Comment : termite on Thu Jul 20 20:52:11 CEST 2017

--- installation/310-install-termite-v1.sh
+++ installation/310-install-termite-v1.sh
@@ -9,8 +9,8 @@ set -e
 #installation of termite
 
 
-git clone --recursive https://github.com/thestinger/termite.git
-git clone https://github.com/thestinger/vte-ng.git
+git clone --recursive https://github.com/thestinger/termite.git /tmp/termite
+git clone https://github.com/thestinger/vte-ng.git /tmp/vte-ng
 sudo apt-get install -y \
 	g++ \
 	libgtk-3-dev \
@@ -26,12 +26,12 @@ sudo apt-get install -y \
 	gperf
 
 echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
+cd /tmp
 cd vte-ng && ./autogen.sh && make && sudo make install
 cd ../termite && make && sudo make install
 sudo ldconfig
-sudo mkdir -p /lib/terminfo/x; sudo ln -s \
-/usr/local/share/terminfo/x/xterm-termite \
-/lib/terminfo/x/xterm-termite
+sudo mkdir -p /lib/terminfo/x
+sudo ln -sf /usr/local/share/terminfo/x/xterm-termite  /lib/terminfo/x/xterm-termite
 
 
 
@@ -39,6 +39,6 @@ sudo mkdir -p /lib/terminfo/x; sudo ln -s \
 echo
 echo
 echo "#########################################################"
-echo "Everything to work comfortably in i3 has been installed"
+echo "Termite is installed"
 echo "#########################################################"
 sleep 1
