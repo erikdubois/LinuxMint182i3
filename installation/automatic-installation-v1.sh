4c34245b0c3d1120e28418fd898b9040d9da1daa
Erik Dubois <erik.dubois@gmail.com>
2017-07-20 20:58:12 +0200

    Comment : automatic script update on Thu Jul 20 20:58:12 CEST 2017

--- installation/automatic-installation-v1.sh
+++ installation/automatic-installation-v1.sh
@@ -20,8 +20,6 @@ sh 100-install-dependencies-v1.sh
 sh 110-install-xcb-util-xrm-v1.sh
 sh 120-install-i3-gaps-next-v1.sh
 
-sh 200-install-j4-dmenu-desktop-v1.sh
-
 sh 300-install-extra-software-v1.sh
 
 sh 400-install-fonts-v1.sh
