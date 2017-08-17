d2daa5e7f23b9588c43a5dec19f84643145c470c
PROger4ever <proger4ever@gmail.com>
2017-08-17 09:51:12 +0500

    Comment : Comment autoinstallation of themes/install-gtk-arc-theme-v1.sh (it breaks system boot-up) on Thu Aug 17 09:51:12 +05 2017

--- installation/420-install-themes-v1.sh
+++ installation/420-install-themes-v1.sh
@@ -9,7 +9,7 @@ set -e
 
 bash themes/theme-arc-colora-collection-3.4-v1.sh
 bash themes/theme-mint-y-colora-collection-3.4-v1.sh
-bash themes/install-gtk-arc-theme-v1.sh
+# bash themes/install-gtk-arc-theme-v1.sh
 
 
 
